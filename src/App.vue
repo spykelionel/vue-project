4157a3e26db536e217a43ad167845a3770324019
spykelionel <social.lionel@gmail.com>
2024-02-24 20:19:54 +0100

    components

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,15 @@
 <script>
+import ButtonCounter from "./components/ButtonCounter.vue";
 export default {
-  data() {
-    return {
-      count: 0,
-      text: "",
-      pick: 1,
-      items: [{ message: "msg1" }, { message: "msg2" }],
-      myText: "",
-    };
-  },
-  methods: {
-    increment() {
-      this.count++;
-    },
-  },
-  mounted() {
-    console.log(`Count is ${this.count}`);
-  },
-  updated() {
-    console.log(`Count is ${this.count}`);
-  },
-  computed: {
-    isCountEven() {
-      return (this.isEven = this.count % 2 == 0 ? true : false);
-    },
+  components: {
+    ButtonCounter,
   },
 };
 </script>
 
 <template>
-  <button @click="increment">Count is {{ count }}</button>
-  <p v-if="isCountEven">You can only see me if count is even.</p>
-  <p v-else>Count is Odd</p>
-
-  <div>
-    <p>List rendering1</p>
-    <ul>
-      <li v-for="({ message }, index) in items" :key="message">
-        {{ index }} - {{ message }}
-      </li>
-    </ul>
-  </div>
-
-  <div>
-    <p>Inputs</p>
-    <p>Text: {{ myText }}</p>
-    <input v-model.trim="myText" />
-  </div>
-  <div>
-    <p>{{ `Pick: ${pick}` }}</p>
-    <input type="radio" v-model="pick" :value="1" />
-    <input type="radio" v-model="pick" :value="2" />
-  </div>
+  <ButtonCounter />
 </template>
-
 <style scoped>
 header {
   line-height: 1.5;
